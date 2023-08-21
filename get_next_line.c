2c74718d414eb697fc75b7a106d0e86908d36b92
Gabriela De Sa <gde-sa@c3r4p1.42sp.org.br>
2023-08-21 18:08:33 -0300

    comecando a gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,13 +6,45 @@
 /*   By: gde-sa <gde-sa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 16:06:06 by gde-sa            #+#    #+#             */
-/*   Updated: 2023/08/17 16:45:55 by gde-sa           ###   ########.fr       */
+/*   Updated: 2023/08/21 17:59:08 by gde-sa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+#include <stdio.h>
+
+// verifica a qantidade de palavras para o malloc
+// lista encadeada
+// retorna a lista
+static char	*line_find(char buffer)
+{
+	printf("%c", buffer);
+}
+
+// percorrer a sring ate o \n dependendo do buffer _size
+static char	run_line(int fd)
+{
+	static char	buffer;
+
+	read(fd, buffer, BUFFER_SIZE);
+	if (ft_strchr(buffer, '\n') != NULL)
+		line_find(buffer);
+	
+}
 
 char	*get_next_line(int fd)
 {
-	
+	if (fd < 0 || BUFFER_SIZE == 0)
+		return (NULL);
+	run_line(fd);
 }
+
+
+int	main(void)
+{
+	int		file;
+
+	file = open("arquivo.txt", O_RDONLY);
+	get_next_line(file);
+}
+
--- get_next_line.h
+++ get_next_line.h
@@ -6,17 +6,28 @@
 /*   By: gde-sa <gde-sa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 11:25:20 by gde-sa            #+#    #+#             */
-/*   Updated: 2023/08/17 16:44:58 by gde-sa           ###   ########.fr       */
+/*   Updated: 2023/08/21 17:49:02 by gde-sa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
-#ifndef BUFFER_SIZE
-# define BUFFER_SIZE
+# include <fcntl.h>
+# include <stdlib.h>
 
+typedef struct s_line
+{
+	void			*content;
+	struct t_line	*next;
+}	t_line;
+
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10
 
 char			*get_next_line(int fd);
+char			*ft_strchr(char *s, int c);
 
+# endif
 #endif
