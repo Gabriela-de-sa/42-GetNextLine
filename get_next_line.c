eab9db928570409bd6b03f503bf81af41c1751d5
Gabriela De Sa <gde-sa@c3r2p5.42sp.org.br>
2023-08-29 18:18:49 -0300

    rolvido problemas de final de linha e backup zerar variavel

--- get_next_line.c
+++ get_next_line.c
@@ -3,82 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabriela <gabriela@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gde-sa <gde-sa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/17 16:06:06 by gde-sa            #+#    #+#             */
-/*   Updated: 2023/08/28 18:32:43 by gabriela         ###   ########.fr       */
+/*   Updated: 2023/08/29 18:10:16 by gde-sa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
 #include <stdio.h>
 
-char	*ft_before_break(char *line)
+char	*ft_substr(char *s, unsigned int start, size_t len)
 {
-	char	*str;
-	int		index;
-	char	*return_line;
+	char	*substring;
+	size_t	len_s;
+	size_t	i;
 
-	str = line;
-	index = 0;
-	while (str[index] && str[index] != '\n')
-		index++;
-	if (str[index] == '\0')
+	len_s = ft_strlen(s);
+	if (start >= len_s)
+		return (ft_strdup(""));
+	if (s == 0)
+		return (NULL);
+	if (len >= len_s)
+		len = len_s - start;
+	substring = (char *) malloc(len + 1);
+	if (substring == NULL)
+		return (NULL);
+	i = 0;
+	while (s[i] && i <= len)
 	{
-		return_line = (char *) malloc(index + 1);
-		if (return_line == NULL)
-			return (NULL);
-		return_line = ft_strncpy(return_line, str, index);
+		substring[i] = s[i + start];
+		i++;
 	}
-	else
-	{
-		return_line = (char *) malloc(index + 1);
-		if (return_line == NULL)
-			return (NULL);
-		return_line = ft_strncpy(return_line, str, index);
-	}
-	printf("%s", return_line);
-	return (return_line);
+	substring[i] = '\0';
+	return (substring);
 }
 
-/*char	*ft_after_break(char *str, int index, char *backup)
+char	*ft_return_line(char *line, char **backup)
 {
-	int		len_str;
-	char	*temp_backup;
-
-	len_str = ft_strlen(str);
-	temp_backup = backup;
-	while (index <= len_str)
-	{
-		temp_backup[index] = str[index];
-		index++;
-	}
-	printf("%s", temp_backup);
-	return (temp_backup);
-}*/
-
-char	*ft_return_line(char *line, char *backup)
-{
-	char	*str;
 	int		index;
-	int		len_str;
 	char	*entire_line;
 
-	str = line;
 	index = 0;
-	while (str[index] && str[index] != '\n')
-		index++;
-	len_str = ft_strlen(str);
-	if (len_str >= index)
+	index = ft_strchr(line);
+	entire_line = ft_substr(line, 0, index);
+	if (index >= 0)
+		(*backup) = ft_strdup(&line[index + 1]);
+	else
 	{
-		while (index <= len_str)
-		{
-			backup[index] = str[index];
-			index++;
-		}
+		free(*backup);
+		*backup = NULL;
 	}
-	entire_line = ft_before_break(str);
-	printf("%s", entire_line);
 	return (entire_line);
 }
 
@@ -94,31 +69,33 @@ char	*get_next_line(int fd)
 	if (!backup)
 		backup = ft_strdup("");
 	line = ft_strdup(backup);
-	buffer = malloc(BUFFER_SIZE + 1 * sizeof(char));
+	buffer = malloc((BUFFER_SIZE + 1) * sizeof(char));
 	if (buffer == NULL)
 		return (NULL);
 	read_bytes = 1;
 	while (read_bytes > 0 && !(ft_strchr(line) >= 0))
 	{
 		read_bytes = read(fd, buffer, BUFFER_SIZE);
-		if (read_bytes < 0)
+		if (read_bytes == -1)
 			return (NULL);
 		buffer[read_bytes] = '\0';
 		line = ft_strjoin(line, buffer);
 	}
-	return (ft_return_line(line, backup));
+	if (line[0] == '\0')
+		return (NULL);
+	return (ft_return_line(line, &backup));
 }
 
-int	main(void)
+/*int	main(void)
 {
 	int		file;
 	int		i;
 
 	file = open("arquivo.txt", O_RDONLY);
 	i = 0;
-	while (i < 10)
+	while (i < 15)
 	{
-		get_next_line(file);
+		printf("%s", get_next_line(file));
 		i++;
 	}
-}
+}*/
--- get_next_line.h
+++ get_next_line.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gabriela <gabriela@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gde-sa <gde-sa@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/16 11:25:20 by gde-sa            #+#    #+#             */
-/*   Updated: 2023/08/28 18:27:03 by gabriela         ###   ########.fr       */
+/*   Updated: 2023/08/29 18:09:47 by gde-sa           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,20 @@
 # include <unistd.h>
 # include <fcntl.h>
 # include <stdlib.h>
-# define BUFFER_SIZE 8
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10000
+# endif
 
 char			*get_next_line(int fd);
 int				ft_strchr(char *s);
 size_t			ft_strlen(char *x);
 char			*ft_strdup(char *s);
 char			*ft_strjoin(char *s1, char *s2);
-char			*ft_return_line(char *line, char *backup);
+char			*ft_return_line(char *line, char **backup);
 char			*ft_strncpy(char *dest, char *src, size_t n);
-char			*ft_before_break(char *line);
 char			*ft_strncpy(char *dest, char *src, size_t n);
+char			*ft_substr(char *s, unsigned int start, size_t len);
+
 
 #endif
